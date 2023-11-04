775e6e642469beb071e0355d4aaa36d9fa6cce1d
EvstratovRG <romgo95@gmail.com>
2023-11-04 17:19:20 +0300

    pagin

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -302,5 +302,5 @@ class FollowSerializer(GetUserSerializer):
         recipes = obj.recipes.all()
         if recipes_limit:
             recipes = recipes[:int(recipes_limit)]
-        serializer = NotDetailRecipeSerializer(many=True, allow_null=True)
+        serializer = NotDetailRecipeSerializer(recipes, many=True, allow_null=True)
         return serializer.data
